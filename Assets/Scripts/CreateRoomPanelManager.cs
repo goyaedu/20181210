4cd8959f1c930ae91a9feea5f75547358450c090
Junil Ko <goyadb@outlook.com>
2018-12-11 12:02:40 +0900

    프로젝트 시작

---
+++ Assets/Scripts/CreateRoomPanelManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoomPanelManager : MonoBehaviour
+{
+    public InputField roomNameInputField;
+    public Button okButton;
+    public Button cancelButton;
+    public Text errorText;
+
+    public delegate void CreateRoomDelegate(string roomName);
+    public CreateRoomDelegate createRoomDelegate;
+
+	void Start ()
+    {
+        errorText.text = "";
+        okButton.interactable = false;
+        roomNameInputField.onValueChanged.
+            AddListener(delegate { CheckRoomName(); });
+	}
+
+    void CheckRoomName() {
+        if (!string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            okButton.interactable = true;
+        }
+        else
+        {
+            okButton.interactable = false;
+        }
+    }
+
+    public void OnClickOKButton()
+    {
+        errorText.text = "";
+
+        if (createRoomDelegate == null)
+        {
+            return;
+        }
+
+        okButton.interactable = false;
+        cancelButton.interactable = false;
+        roomNameInputField.interactable = false;
+
+        createRoomDelegate(roomNameInputField.text);
+    }
+
+    public void OnClickCancelButton()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void CreateRoomFailed()
+    {
+        errorText.text = "방 생성 실패.";
+        cancelButton.interactable = true;
+        roomNameInputField.interactable = true;
+        roomNameInputField.text = "";
+    }
+
+    public void CreateRoomSuccess()
+    {
+        Destroy(this.gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+
+public class IntroManager : PunBehaviour
+{
+    public Button createButton;                 // 방 생성 버튼
+    public GameObject createRoomPanelPrefab;    // 방 생성 팝업
+    public Canvas canvas;
+
+    string serverVer = "0.1";
+    CreateRoomPanelManager createRoomPanelManager;
+
+    private void Awake()
+    {
+        createButton.interactable = false;
+
+        // Photon 초기화
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        PhotonNetwork.autoJoinLobby = true;
+        PhotonNetwork.automaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    // 서버 접속
+    void Connect()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            PhotonNetwork.ConnectUsingSettings(serverVer);
+        }
+    }
+
+    void CreateRoom(string roomName)
+    {
+        if (string.IsNullOrEmpty(roomName))
+        {
+            return;
+        }
+
+        RoomOptions option = new RoomOptions();
+        option.IsOpen = true;
+        option.IsVisible = true;
+        option.MaxPlayers = 10;
+
+        PhotonNetwork.CreateRoom(roomName, option, TypedLobby.Default);
+    }
+
+    public void OnClickCreateButton()
+    {
+        if (createRoomPanelManager == null)
+        {
+            GameObject createRoomPanelGameObject = Instantiate(createRoomPanelPrefab);
+            createRoomPanelGameObject.transform.SetParent(canvas.transform, false);
+            createRoomPanelManager =
+                createRoomPanelGameObject.GetComponent<CreateRoomPanelManager>();
+            createRoomPanelManager.createRoomDelegate = CreateRoom;
+        }
+    }
+
+    public override void OnJoinedLobby()
+    {
+        // TODO: Create Room 버튼 활성화
+        createButton.interactable = true;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        // TODO: Game Scene으로 전환
+        // 방 만들기 위한 팝업 창 닫기
+
+        Debug.Log("## OnJoinedRoom()");
+    }
+
+    public override void OnPhotonJoinRoomFailed(object[] codeAndMsg)
+    {
+        // TODO: 방을 선택할 수 있게 UI 갱신
+    }
+
+    public override void OnPhotonCreateRoomFailed(object[] codeAndMsg)
+    {
+        // TODO: 유저에게 방 생성 실패 알리기
+    }
+}
